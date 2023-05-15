aa951080e1fec760c2cf2491d91ab6470fb8eeb9
Joe Desmond <desmonji@bc.edu>
2023-05-15 01:27:31 +0000

    added type table to gen_tac function signature

--- src/codegen.h
+++ src/codegen.h
@@ -1,38 +1,7 @@
 #ifndef CODEGEN_H
 #define CODEGEN_H
 
-#include <map>
-
-#include "ast.h"
 #include "tac.h"
-
-/**
- * This class owns all Typename ptrs, so be sure to call clone() before inserting
- * a typename into the map
- */
-class TypeTable {
-    std::map<std::string, Typename *> types;
-
-    TypeTable() : types({}) {}
-
-    ~TypeTable() {
-        for (std::pair<const std::string, Typename *> &item : types) {
-            delete item.second;
-        }
-    }
-
-    void put(std::string name, Typename * typ) {
-        types[name] = typ;
-    }
-
-    Typename * get(std::string name) {
-        if (types.count(name)) {
-            return types[name];
-        }
-
-        return nullptr;
-    }
-};
 
 class CodeGenerator {
 
