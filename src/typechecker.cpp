3b8d1e32b58576383fa32470fcb09303c23e9ad3
AlecBailey12 <117847138+AlecBailey12@users.noreply.github.com>
2023-05-15 05:11:40 -0700

    Please typechecker.cpp

--- src/typechecker.cpp
+++ src/typechecker.cpp
@@ -910,7 +910,7 @@ void FuncDecl::typecheck(SymbolTable * symtable, SourceErrors &errors) const {
     const size_t num_stmts = body->statements.size();
     const bool ends_with_ret = num_stmts > 0 && body->statements[num_stmts - 1]->get_kind() == ReturnStatement::kind;
 
-    if ((!ret_type->type_equals(&void_type, symtable) && !ends_with_ret) || (!ret_type->type_equals(&Please_type, symtable) && !ends_with_ret)) {
+    if (!ends_with_ret && (!ret_type->type_equals(&Please_type, symtable) || !ret_type->type_equals(&void_type, symtable))) {
         CompilerError error(x::NULL_LOC, "Missing return statement at end of non-Please/void function", Error);
         errors.type_errors.push_back(error);
     }
